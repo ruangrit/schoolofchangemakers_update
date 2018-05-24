7bba9cc9dd2496d09f2764c43ff96e08c5cdec1d
Rito <ruangrit@opendream.co.th>
2018-05-24 15:23:32 +0700

    problem check all

--- sites/all/modules/changemakers/script_checkbox.js
+++ sites/all/modules/changemakers/script_checkbox.js
@@ -1,6 +1,58 @@
 $ = jQuery;
 $(document).ready(function () {
 
-	console.log('ddddddddd');
-	console.log(Drupal.settings.changemakers.problems);
+	var problems = Drupal.settings.changemakers.problems;
+	var idOfProblemAll = 'edit-field-problem-interest-und-171';
+	$.each(problems, function( index, value ) {
+
+		$('#edit-field-problem-interest-und-' + index).addClass('parent_checkbox');
+		$('#edit-field-problem-interest-und-' + index).addClass('parent_checkbox_' + index);
+
+		$.each(value, function( index_child, value_child ) {
+
+			$('#edit-field-problem-interest-und-' + value_child).addClass('child_checkbox');
+			$('#edit-field-problem-interest-und-' + value_child).addClass('child_checkbox_' + index);
+			$('#edit-field-problem-interest-und-' + value_child).attr({'parentid': index});
+		});
+
+	});
+
+	//============================= 
+    $('.parent_checkbox').change(function () {
+        if(this.checked) {
+            $('.child_checkbox_'+this.value).prop("checked", true );
+        }
+        else {
+            $('.child_checkbox_'+this.value).prop("checked", false);
+            $('#' + idOfProblemAll).prop("checked", false );
+        }
+
+    });
+
+    $('.child_checkbox').click(function () {
+
+        if(!this.checked) {
+            var parentId = $(this).attr('parentid');
+            $('.parent_checkbox_'+parentId).prop("checked", false);
+            $('#' + idOfProblemAll).prop("checked", false );
+        }
+    });
+    // Check All
+    $('#' + idOfProblemAll).click(function () {
+        if(this.checked) {
+        	$.each(problems, function( index, value ) {
+	            $('.parent_checkbox_'+index).prop("checked", true );
+	            $('.parent_checkbox_'+index).trigger('change');
+        	});
+        }
+        else {
+        	$.each(problems, function( index, value ) {
+	            $('.parent_checkbox_'+index).prop("checked", false);
+	            $('.parent_checkbox_'+index).trigger('change');
+        	});
+        }
+    });
+
+
+
 });
