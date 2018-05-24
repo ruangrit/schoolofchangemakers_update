f92743878d8c35cc0516c517a3a1321d3ad8c6a1
Rito <ruangrit@opendream.co.th>
2018-05-24 16:10:20 +0700

    check all skills

--- sites/all/modules/changemakers/script_checkbox.js
+++ sites/all/modules/changemakers/script_checkbox.js
@@ -3,19 +3,34 @@ $(document).ready(function () {
 
 	var problems = Drupal.settings.changemakers.problems;
 	var idOfProblemAll = 'edit-field-problem-interest-und-171';
-	$.each(problems, function( index, value ) {
+	var problemFieldName = 'problem-interest'
 
-		$('#edit-field-problem-interest-und-' + index).addClass('parent_checkbox');
-		$('#edit-field-problem-interest-und-' + index).addClass('parent_checkbox_' + index);
+	var skills = Drupal.settings.changemakers.skills;
+	var idOfSkillAll = 'edit-field-event-skill-interest-und-173';
+	var skillFieldName = 'event-skill-interest'
 
-		$.each(value, function( index_child, value_child ) {
 
-			$('#edit-field-problem-interest-und-' + value_child).addClass('child_checkbox');
-			$('#edit-field-problem-interest-und-' + value_child).addClass('child_checkbox_' + index);
-			$('#edit-field-problem-interest-und-' + value_child).attr({'parentid': index});
+	loopAddClass(problems, problemFieldName);
+	loopAddClass(skills, skillFieldName);
+
+	function loopAddClass(taxonomyList, fieldName) {
+		console.log(taxonomyList);
+
+		$.each(taxonomyList, function( index, value ) {
+
+			$('#edit-field-'+ fieldName +'-und-' + index).addClass('parent_checkbox');
+			$('#edit-field-'+ fieldName +'-und-' + index).addClass('parent_checkbox_' + index);
+
+			$.each(value, function( index_child, value_child ) {
+
+				$('#edit-field-'+ fieldName +'-und-' + value_child).addClass('child_checkbox');
+				$('#edit-field-'+ fieldName +'-und-' + value_child).addClass('child_checkbox_' + index);
+				$('#edit-field-'+ fieldName +'-und-' + value_child).attr({'parentid': index});
+			});
+
 		});
 
-	});
+	}
 
 	//============================= 
     $('.parent_checkbox').change(function () {
@@ -24,8 +39,11 @@ $(document).ready(function () {
         }
         else {
             $('.child_checkbox_'+this.value).prop("checked", false);
-            $('#' + idOfProblemAll).prop("checked", false );
+	        var parentGroup = $(this).parents('.form-checkboxes');
+	        var allCheckbox = parentGroup.find('input').first();
+            allCheckbox.prop("checked", false );
         }
+
 
     });
 
@@ -34,25 +52,29 @@ $(document).ready(function () {
         if(!this.checked) {
             var parentId = $(this).attr('parentid');
             $('.parent_checkbox_'+parentId).prop("checked", false);
-            $('#' + idOfProblemAll).prop("checked", false );
-        }
-    });
-    // Check All
-    $('#' + idOfProblemAll).click(function () {
-        if(this.checked) {
-        	$.each(problems, function( index, value ) {
-	            $('.parent_checkbox_'+index).prop("checked", true );
-	            $('.parent_checkbox_'+index).trigger('change');
-        	});
-        }
-        else {
-        	$.each(problems, function( index, value ) {
-	            $('.parent_checkbox_'+index).prop("checked", false);
-	            $('.parent_checkbox_'+index).trigger('change');
-        	});
+	        var parentGroup = $(this).parents('.form-checkboxes');
+	        var allCheckbox = parentGroup.find('input').first();
+            allCheckbox.prop("checked", false );
         }
     });
 
+    //===========  Check All
+
+    $('#' + idOfProblemAll).click(function () {
+    	loopCheckAllChange(problems, this.checked);
+    });
+    $('#' + idOfSkillAll).click(function () {
+    	loopCheckAllChange(skills, this.checked);
+    });
+
+    function loopCheckAllChange(taxonomyList, checked) {
+
+    	$.each(taxonomyList, function( index, value ) {
+            $('.parent_checkbox_'+index).prop("checked", checked);
+            $('.parent_checkbox_'+index).trigger('change');
+    	});
+
+    }
 
 
 });
